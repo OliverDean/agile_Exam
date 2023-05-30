4876bdb87f37cccf5454d895d60e0ce31d34b09b
Oliver <21307131@student.uwa.edu.au>
2023-05-30 20:58:20 +0800

    fin

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     let stepsData = [
         {
             title: "Virtual Environment Setup",
-            text: "",
-            code: ["python -m venv venv","venv\\Scripts\\activate","pip install -r requirements.txt"],
-            image: "",
+            text: "After downloading the Git repository and accessing it through an integrated development environment (IDE) like Visual Studio Code, please proceed by executing the following code snippets in a terminal running with administrator privileges. These code snippets are designed to set up a Python virtual environment. The virtual environment isolates our Flask application, keeping it modular and self-contained, reducing potential conflicts between its dependencies and other Python applications. The requirements.txt file lists all the necessary Python libraries our app needs to function correctly. Upon activating the virtual environment with venv\\Scripts\\activate, the command pip install -r requirements.txt ensures the proper installation of these dependencies, enabling the smooth initialization and running of our Flask app. Please remember to run these commands from the Login page directory.",
+            code: ["python -m venv venv","venv\\Scripts\\activate","pip install -r requirements.txt","flask run"],
+            image: "./venv.jpg",
             aside: {
                 label: "",
-                note: ""
+                note: "CAT",
+                'aside-image': './Alicecat.png'
             }
         },
         {
@@ -17,10 +18,32 @@ document.addEventListener("DOMContentLoaded", function () {
             image: "./Re_Start_Db.jpg",
             aside: {
                 label: "Object Relational Mapping",
-                note: "SQLAlchemy allows us to work with Relational Databases in Python efficiently and provides us with a high degree of abstraction. Models (or tables in the database) are defined as Python classes, and SQLAlchemy then allows us to interact with these tables as if they were Python objects. Creating a new row in a table is as simple as creating a new instance of a model and adding it to the session. SQLAlchemy also simplifies the task of writing complex SQL queries. In our project, for instance, we use SQLAlchemy in the 'history' route to fetch and filter questions and the user's answers from the database. This is done using methods like `Question.query.all()` or `UserQuestionAnswer.query.filter_by()`, which behind the scenes, convert these commands to raw SQL queries and execute them.",
+                note: "SQLAlchemy allows us to work with Relational Databases in Python efficiently and provides us with a high degree of abstraction. Models (or tables in the database) are defined as Python classes, and SQLAlchemy then allows us to interact with these tables as if they were Python objects. Creating a new row in a table is as simple as creating a new instance of a model and adding it to the session. SQLAlchemy also simplifies the task of writing complex SQL queries. In our project, for instance, we use SQLAlchemy in the 'history' route to fetch and filter questions and the user's answers from the database. This is done using methods like `Question.query.all()` or `UserQuestionAnswer.query.filter_by()`, which behind the scenes, convert these commands to raw SQL queries and execute them. The following is the code for the ./history route which queries the database for the user's answers to a specific question that contains the string in the search feild.",
                 'aside-image': './History_code.jpg'
             }
+        },
+        {
+            title: "Running the App",
+            text: "Once the virtual environment is set up and the database is initialized, the app can be run. To do this, simply run the following command from the Login page directory. The app will then be accessible from a web browser at the address http://localhost:5000/. The app can be stopped by pressing CTRL+C in the terminal. To restart the app, simply run the same command again. Register a new user or log in with the following credentials: Username: test_user, Password: test_password.",
+            code: ["flask run"],
+            image: "./Flask_Run.jpg",
+            aside: {
+                label: "Routing",
+                note: "In a Flask application, routes play a critical role by handling HTTP requests and responses between the client and server in a stateless architecture. Each route is a Python function that gets executed when a specific URL is accessed. This project's routes (e.g., '/login', '/register', '/logout') manage user authentication and session tracking. For instance, the '/login' route responds to both GET and POST requests. It processes the incoming user credentials and authenticates the user by querying the database. If the credentials are valid, it initializes a session for the authenticated user. The '/register' route also handles GET and POST requests, but instead of authenticating, it validates and processes user data for a new account. It checks constraints such as password length, uniqueness of the username, validity of the email, and ensures passwords match. Once data is validated, it stores the new user in the database. This routing mechanism is an example of Flask's functionality and simplicity, it allows for the efficient management of stateless client-server communication.",
+                'aside-image': './Route_login.jpg'
+            }
+        },        
+        {
+            title: "Testing",
+            text: "Automated testing in our app improves maintainability, removing the necessity for the Flask app to be continuously running during the testing phase. Each test initiates an independent instance of the application, focusing either on unit testing or Selenium-based browser testing. Our Selenium tests are divided into two distinct runs, examining user registration and login functionalities, and testing chat functionality and all the routes that can be accessed. During the Selenium tests, a random string of characters is generated, creating unique usernames and passwords for each instance. Unit testing refers to the database and server-side architecture. These tests include verifying multiple responses from a single user and executing methods like test_user_question_answer_without_user_or_question, test_multiple_sessions_for_user, test_create_session, and others. To start the Selenium and Python Unit tests, simply run the specified command from the Login page directory.",
+            code: ["python autoSelenium.py", "python -m unittest testing.unitTests"],
+            image: "./Selenium_test.jpg",
+            aside: {
+                label: "",
+                note: ""
+            }
         }
+        
     ];
 
     let stepsContainer = document.getElementById("steps");
@@ -84,7 +107,7 @@ function createStepElement(step) {
         ${asideBlock}
         <p>${step.text}</p>
         ${codeBlock}
-        <img src="${step.image}" alt="${step.title}">
+        <img class="image-container" src="${step.image}" alt="${step.title}">
     `;
 
     return stepElement;
